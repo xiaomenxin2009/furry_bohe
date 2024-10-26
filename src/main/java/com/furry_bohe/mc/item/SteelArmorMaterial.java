44e112210258d16c8d87e4ea26bed1b1e8f74a2c
xiaomenxin2009 <wuyancen2789@163.com>
2024-10-26 13:38:46 +0800

    first

--- src/main/java/com/furry_bohe/mc/item/SteelArmorMaterial.java
+++ src/main/java/com/furry_bohe/mc/item/SteelArmorMaterial.java
@@ -1,4 +1,55 @@
 package com.furry_bohe.mc.item;
 
-public class SteelArmorMaterial {
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public class SteelArmorMaterial implements ArmorMaterial {
+
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {A, B, C, D};
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+
+    // 其中A是头盔，B是护腿，C是胸甲，D是靴子。
+        // 例如，皮革使用{1, 2, 3, 1}，钻石和下界合金使用{3, 6, 8, 3}
 }
